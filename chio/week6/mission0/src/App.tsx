2e3a6a39e5cce8e8205542190a237d0832ec9b04
skyhjune <skyhjune@naver.com>
2026-05-05 15:57:29 +0900

    FEAT : 6주차 실습 구현

---
+++ chio/week6/mission0/src/App.tsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import { useCustomFetch } from "./hooks/useCustomFetch";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+function App() {
+  const { data, isPending, isError } = useCustomFetch<User>(
+    "https://jsonplaceholder.typicode.com/users/1",
+  );
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+  if (isPending) {
+    return <div>Loading</div>;
+  }
+
+  return (
+    <>
+      <h1>Query</h1>
+      {data?.name}
+    </>
+  );
+}
+
+export default App;
