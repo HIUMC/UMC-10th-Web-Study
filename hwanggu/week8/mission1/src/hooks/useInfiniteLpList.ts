93b33458bf26170f68190d15db5afb1116b57430
D5-wq <hyjd5@naver.com>
2026-05-16 23:41:39 +0900

    FEAT: 8주차 미션1 제출

---
+++ hwanggu/week8/mission1/src/hooks/useInfiniteLpList.ts
@@ -0,0 +1,12 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getLpList } from "../apis/lp";
+
+export function useInfiniteLpList(order: "asc" | "desc") {
+  return useInfiniteQuery({
+    queryKey: ["lps", order],
+    queryFn: ({ pageParam = 0 }) => getLpList(order, pageParam),
+    getNextPageParam: (lastPage) =>
+      lastPage.hasNext ? lastPage.nextCursor : undefined,
+    initialPageParam: 0,
+  });
+}
