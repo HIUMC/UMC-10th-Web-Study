ae86ed646257d48c7d9125f7a69641694c3ba971
SongHyewon <songhw1215@g.hongik.ac.kr>
2026-05-26 12:32:54 +0900

    FEAT: 9주차 실습 제출

---
+++ hi/week9/useReducer/src/App.tsx
@@ -0,0 +1,18 @@
+import Counter from "./components/Counter";
+import FormExample from "./components/FormExample";
+
+function App() {
+  return (
+    <div>
+      <h1>useReducer 실습</h1>
+
+      <Counter />
+
+      <hr />
+
+      <FormExample />
+    </div>
+  );
+}
+
+export default App;
