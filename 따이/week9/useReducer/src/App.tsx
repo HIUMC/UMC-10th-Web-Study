c2e470c34571463fb7e978b7d275782f2661079a
itaekyung <taeyun1411@gmail.com>
2026-05-28 00:23:28 +0900

    FEAT: 실습 구현

---
+++ 따이/week9/useReducer/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UseReducerCompany from "./useReducer/useReducerCompany";
+
+function App() {
+  return (
+    <>
+      <UseReducerCompany />
+    </>
+  );
+}
+
+export default App;
